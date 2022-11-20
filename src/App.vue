47a6ab768d361249e98e9d432ff7297dd7fdb158
Erjie Zhang <z1832051637@gmail.com>
2022-11-19 19:29:26 -0800

    Created Empty Vue 3 App

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,47 @@
-<template>
-    <a-layout class="main-page">
-        <Navigation />
-        <a-layout>
-            <a-layout style="padding: 0 24px 24px">
-                <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
-                    <router-view></router-view>
-                </a-layout-content>
-            </a-layout>
-        </a-layout>
-    </a-layout>
-</template>
-
-<script setup lang="ts">
-import Navigation from "./views/components/navbar.vue"
-// import SideBar from "./views/components/sidebar.vue"
-
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
-<style scoped lang="css">
-.main-page{
-    height:100%;
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 }
 </style>
